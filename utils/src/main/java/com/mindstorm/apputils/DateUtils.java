f4ee4af0e16659a4baa0ca8dd23df6f4b0654347
Administrator <Administrator@MicroWin10-2108>
2018-03-31 12:37:26 +0800

    增加一个方法

--- utils/src/main/java/com/mindstorm/apputils/DateUtils.java
+++ utils/src/main/java/com/mindstorm/apputils/DateUtils.java
@@ -10,5 +10,10 @@ public class DateUtils {
 		  LocalDate newYear = fromDate.plusYears(1).withDayOfYear(1);
 		  return Days.daysBetween(fromDate, newYear).getDays();
 	}
+	public int daysToNewYear1() {
+	      LocalDate fromDate = new LocalDate();
+		  LocalDate newYear = fromDate.plusYears(1).withDayOfYear(1);
+		  return Days.daysBetween(fromDate, newYear).getDays();
+	}
 
 }
