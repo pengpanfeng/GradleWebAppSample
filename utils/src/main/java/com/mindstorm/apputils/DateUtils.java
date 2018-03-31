6a7300d72c1b1cca19ddbe749a589b6bb5b9ae52
Administrator <Administrator@MicroWin10-2108>
2018-03-31 14:15:34 +0800

    测试冲突

--- utils/src/main/java/com/mindstorm/apputils/DateUtils.java
+++ utils/src/main/java/com/mindstorm/apputils/DateUtils.java
@@ -30,5 +30,15 @@ public class DateUtils {
 		  LocalDate newYear = fromDate.plusYears(1).withDayOfYear(1);
 		  return Days.daysBetween(fromDate, newYear).getDays();
 	}
+	public int daysToNewYear5() {
+	      LocalDate fromDate = new LocalDate();
+		  LocalDate newYear = fromDate.plusYears(1).withDayOfYear(1);
+		  return Days.daysBetween(fromDate, newYear).getDays();
+	}
+	public int daysToNewYear6() {
+	      LocalDate fromDate = new LocalDate();
+		  LocalDate newYear = fromDate.plusYears(1).withDayOfYear(1);
+		  return Days.daysBetween(fromDate, newYear).getDays();
+	}
 
 }
